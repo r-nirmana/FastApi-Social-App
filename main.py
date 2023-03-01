2ba7d86b0729a65576c70029f1a524cb3c7f2924
Ranga <nirmanperera8@gmail.com>
2023-03-01 21:33:14 +0530

    Adding post request

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
+from fastapi.params import Body
+from pydantic import BaseModel
 
 app = FastAPI()
 
+#define a class how a post should look like
+
+class Post(BaseModel):
+    title : str
+    content : str
 
 #Path Operation or the Route
 
@@ -12,3 +19,8 @@ async def root():
 @app.get("/posts")
 def get_posts():
     return {"message": "These are your posts"}
+
+@app.post("/createposts")
+def create_posts(new_post: Post):
+    print(new_post)
+    return{"Data": "New Post"}
