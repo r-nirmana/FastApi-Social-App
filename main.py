8228ed38cce2b15b922d8087500224ea5dcd621e
Ranga <nirmanperera8@gmail.com>
2023-03-04 22:58:36 +0530

    testing CRUD operations

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ while True:
     try:
         # environment variables to be added
         conn = psycopg2.connect(host='localhost', database = 'fastapi', user = 'postgres', 
-        password = 'Adopado@77', cursor_factory=RealDictCursor)
+        password = 'password', cursor_factory=RealDictCursor)
 
         cursor = conn.cursor()
         print("Database Connection was succesfull")
